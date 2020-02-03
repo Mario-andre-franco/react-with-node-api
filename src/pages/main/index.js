ba495674a520f3f640b11954e788aca0b0ea8b88
marioandrecarvalho <mario.andre.franco@gmail.com>
2020-02-03 09:22:54 -0300

    First commit

---
+++ src/pages/main/index.js
@@ -0,0 +1,58 @@
+import React, {Component } from 'react'
+import api from '../../services/api'
+import './styles.css'
+export default class Main extends Component{
+    state = {
+        products: [],
+        productsInfo: {},
+        page: 1,
+    }
+    componentDidMount() {
+        this.loadProducts()
+    }
+
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`)
+        const {docs, ...productsInfo} = response.data
+        this.setState({products: docs, productsInfo, page})
+    }
+
+    prevPage = () => {
+        const { page, productsInfo} = this.state
+
+        if (page === 1) return
+
+        const pageNumber = page - 1
+
+        this.loadProducts(pageNumber)
+    }
+
+    nextPage = () => {
+        const {page, productsInfo} = this.state
+        if (page === productsInfo.pages) return
+
+        const pageNumber = page + 1
+
+        this.loadProducts(pageNumber)
+    }
+
+    render() {
+        const { products, page, productsInfo } = this.state
+        return (
+            <div className="product-list">
+                {products.map(products => (
+                    <article key={products._id}>
+                        <strong> {products.title}</strong>
+                        <p>{products.description}</p>
+
+                        <a href=""> Acessar</a>
+                    </article>
+                ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}> Anterior</button>
+                    <button disabled={page === productsInfo.page} onClick={this.nextPage}> Proximo</button>
+                </div>
+            </div>
+        )
+    }
+}
